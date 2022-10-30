a5cae45c44a5dab306f42e17f7ce004e28422313
Andre J <andre.joubert99@gmail.com>
2022-10-30 19:49:31 +0200

    Update

--- SQlite.py
+++ SQlite.py
@@ -14,16 +14,16 @@ conn = sq.connect("Employees.db")
 
 c = conn.cursor()
 
-# c.execute("""CREATE TABLE employees (
-#     name TEXT,
-#     surname TEXT,
-#     age INTEGER,
-#     occupation TEXT,
-#     email TEXT,
-#     )"""
-# )
+c.execute("""CREATE TABLE employees (
+    name TEXT,
+    surname TEXT,
+    age INTEGER,
+    occupation TEXT,
+    email TEXT,
+    )"""
+)
 
-c.execute("INSERT INTO employees VALUES (name, surname, age, occupation, ?)")
+c.execute("INSERT INTO employees VALUES ("+name+", "+surname+", "+age+", "+occupation+", 'email@gmail.com')")
 
 conn.commit()
 
